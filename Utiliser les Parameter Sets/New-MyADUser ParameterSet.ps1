0d0e273ed739da79c6414a25236d060968d47af0
Lienhard Laurent <laurent.lienhard@polehabitatstrasbourg.fr>
2020-07-13 11:48:38 +0200

    updat

---
+++ Utiliser les Parameter Sets/New-MyADUser ParameterSet.ps1
@@ -0,0 +1,16 @@
+﻿﻿$UserName = @("Utilisateur1.Test", "Utilisateur2.Test", "Utilisateur3.Test")
+$FilePath = '.\Utiliser les Parameter Sets\Utilisateurs.csv'
+
+function New-MyADUser {
+    [CmdletBinding(DefaultParameterSetName = "ByUserName")]
+    param (
+        [Parameter(ParameterSetName='ByUserName')]
+        [System.String[]]$UserName,
+        [Parameter(ParameterSetName = 'ByFileName')]
+        [System.String]$FilePath
+    )
+
+
+
+
+}
---
+++ Utiliser les Parameter Sets/New-MyADUser avec fichier.ps1
@@ -0,0 +1,15 @@
+﻿﻿$FilePath = '.\Utiliser les Parameter Sets\Utilisateurs.csv'
+function New-MyADUser {
+    param (
+        [System.String]$FilePath
+    )
+
+    $UserName = Import-Csv -Path $FilePath
+
+    foreach ($User in $UserName) {
+        New-ADUser -name $User.UserName -GivenName ($User.UserName.Split(".")[0]) -Surname ($User.UserName.Split(".")[1]) -Server srv-ad101 -Credential $CredAdmin
+    }
+
+}
+
+New-MyADUser -FilePath $FilePath
